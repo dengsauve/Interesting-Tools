6bb94f17e1e01e087a153a64fb9ed13050aac6b9
Dennis <dengsauve@yahoo.com>
2016-11-10 13:18:27 -0800

    Updated w/ Reverse Polish Notation Method
    Dailyprogrammer challenge solution, but is handy to keep around.

--- MathTools/Dennis_Math_Lib.py
+++ MathTools/Dennis_Math_Lib.py
@@ -4,7 +4,7 @@
 # Requires list of polynomial coefficients
 # from highest factor to constant in
 # descending order. Also, only one zero.
-
+import math
 
 def synthetic_division(coefficients, zero):
     x = float(coefficients[0])
@@ -20,3 +20,22 @@ def get_coefficients():
         if c != 'f':
             return_list.append(float(c))
     return return_list
+
+def reverse_polish(input_stack):
+    #Requires input_stack to be a list.
+    f = {
+		'+': lambda t, b: b + t,
+		'-': lambda t, b: b - t,
+		'*': lambda t, b: b * t,
+		'/': lambda t, b: b / t,
+		'//':lambda t, b: b // t,
+		'%': lambda t, b: b % t,
+		'^': lambda t, b: b ** t}
+    short_stack = []
+    for i in input_stack:
+        if i == '!':short_stack.append(math.factorial(short_stack.pop()))
+        elif i in f:
+            t, b = short_stack.pop(), short_stack.pop()
+            short_stack.append(f[i](t, b))
+        else:short_stack.append(float(i))
+    return short_stack[0] if len(short_stack) == 1 else 'failed'
