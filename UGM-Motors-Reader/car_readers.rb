bb7dcfcd1050043795db879e137ff61c11ade70b
Dennis <dengsauve@yahoo.com>
2017-04-30 02:38:20 -0700

    Included 3 New Dealers
    Auto Credit Sales, TK Auto Sales, Independent Auto Sales

--- UGM-Motors-Reader/car_readers.rb
+++ UGM-Motors-Reader/car_readers.rb
@@ -35,7 +35,7 @@ class Jennifers < Carlot
   def car_list
     ret_str = ''
     retrieve_cars.each do |car|
-      ret_str += car[0].ljust(5) + car[1].ljust(40, '.') + car[2].rjust(11, '.') + "\t" + car[3] + "\n"
+      ret_str += car[0].ljust(5) + car[1].ljust(40, '.') + car[2].rjust(11, '.') + " > " + car[3] + "\n"
     end
     ret_str
   end
@@ -65,10 +65,111 @@ class UGMmotors < Carlot
   def car_list
     ret_str=''
     retrieve_cars.each do |car|
-      ret_str += car[0].ljust(30, '.') + car[1].rjust(11, '.') + "\t" + car[2] + "\n"
+      ret_str += car[0].ljust(30, '.') + car[1].rjust(11, '.') + " > " + car[2] + "\n"
     end
     ret_str
   end
 
 end
 
+
+class AutoCredit < Carlot
+
+  def retrieve_cars
+    cars, name, url, price = [], '', '', ''
+    @contents.each do |line|
+      if line =~ /<meta\sitemprop="name"\scontent="(.+)"\/>/
+        name = $1[0..29]
+      end
+      if line =~ /<meta\sitemprop="url"\scontent="(.+)"\/>/
+        url = $1
+      end
+      if line =~ /<meta\sitemprop="price"\scontent="(\d+)"\/>/
+        price = $1
+        cars << [name, price, url]
+        name, url, price = '', '', ''
+      end
+    end
+    cars
+  end
+
+  def car_list
+    ret_str = ''
+    retrieve_cars.each do |car|
+      ret_str += car[0].ljust(30, '.') + ('$'+car[1]).rjust(11, '.') + ' > ' + car[2] + "\n"
+    end
+    ret_str
+  end
+
+end
+
+
+class TkAutoSales < Carlot
+
+  def retrieve_cars
+    cars, name, price, url = [], '', '', ''
+    @contents.each do |line|
+      if line =~ /<div><a\sclass="accent-color1"\shref="(.+)"\stitle="(.+)"\starget/
+        url = $1
+        name = $2[0..29]
+      end
+      if line =~ /\$<\/span>(.+)<\/b><\/div>/
+        price = $1
+        cars << [name, price, url]
+      end
+    end
+    cars
+  end
+
+  def car_list
+    ret_str = ''
+    retrieve_cars.each do |car|
+      ret_str += car[0].ljust(30, '.') + ('$'+car[1]).rjust(11, '.') + ' > ' + car[2] + "\n"
+    end
+    ret_str
+  end
+
+end
+
+
+class IndependentAuto < Carlot
+
+  def retrieve_cars
+    cars, name, price, url = [], '', '', ''
+    @contents.each do |line|
+      if line =~ /<a\sid=".*"\shref="(.*)">(.*)<\/a>/
+        name = $2[0..29]
+        url = 'http://www.independentautowa.com/' + $1
+      end
+      if line =~ /(\$\d*,?\d{3})/
+        price = $1
+        cars << [name, price, url]
+      end
+    end
+    cars
+  end
+
+  def car_list
+    ret_str = ''
+    retrieve_cars.each do |car|
+      ret_str += car[0].ljust(30, '.') + car[1].rjust(11, '.') + ' > ' + car[2] + "\n"
+    end
+    ret_str
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# end of class
--- UGM-Motors-Reader/main.rb
+++ UGM-Motors-Reader/main.rb
@@ -3,13 +3,43 @@ require_relative('car_readers')
 
 def union_gospel
   ugm = UGMmotors.new('http://www.ugmmotors.org/inventory')
-  puts ugm.car_list
+  ugm.car_list
 end
 
 def jennifer_auto
-# site: Jennifer's Auto
-# URL:  http://www.jennifersautosales.com/content/inventory.php
-
   jazz = Jennifers.new('http://www.jennifersautosales.com/content/inventory.php')
-  puts jazz.car_list
+  jazz.car_list
 end
+# Auto Credit Sales  http://www.autocreditsales.com/used-cars-spokane-valley-wa?
+
+def auto_credit_sales
+  ret_str = ''
+  (1..30).to_a.each do |page|
+    acl = AutoCredit.new("http://www.autocreditsales.com/used-cars-spokane-valley-wa?page=#{page}")
+    ret_str += acl.car_list
+  end
+  ret_str
+end
+
+def tk_auto
+  ret_str = ''
+  (1..15).to_a.each do |page|
+    tka = TkAutoSales.new("http://www.tkautosalesspokane.com/used-cars-for-sale-spokane?page=#{page}")
+    ret_str += tka.car_list
+  end
+  ret_str
+end
+
+def independent
+  ias = IndependentAuto.new('http://www.independentautowa.com/inventory.aspx?cursort=asc&pagesize=500&ordby=price')
+  ias.car_list
+end
+
+
+
+
+
+
+
+
+# end of main
